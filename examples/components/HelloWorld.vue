5ae5ea89fa795d6e1d3954095cf07ff47fe252fd
xudan <xudan@apowo.com>
2022-03-07 14:34:31 +0800

    demo  test

--- examples/components/HelloWorld.vue
+++ examples/components/HelloWorld.vue
@@ -20,9 +20,9 @@
  <script>
 // import '../../lib/eth-wallet-modal.css'
 // import Base from '../../lib/eth-wallet-modal.umd.min.js'
-import Base from '../../packages/index'
+// import Base from '../../packages/index'
 // import getUrlParameters from 'webpack-build-tools-test';
-// import Base from 'eth-wallet-modal';
+import Base from 'eth-wallet-modal';
 
 import WalletConnectLogo from "../assets/logos/walletconnect-circle.svg";
 
